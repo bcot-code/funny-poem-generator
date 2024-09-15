ea96992cc252b9c3bdf19abc850c89da5cd16943
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-09-15 15:04:38 -0500

    Added handlebar & Typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello Dev!");
+function generateFy(event) {
+  event.preventDefault();
+
+  alert("Processing the funny quote...");
+  new Typewriter("#funny-quote", {
+    strings: "I see your face when I am dreaming",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let funnyPoem = document.querySelector("#funny-poem");
+funnyPoem.addEventListener("submit", generateFy);
