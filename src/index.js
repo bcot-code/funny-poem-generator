d99a555d262c141121f2f293b87e47b99baa103f
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-09-16 12:09:26 -0500

    Added API & gitignore

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-function generateFy(event) {
-  event.preventDefault();
-
-  alert("Processing the funny quote...");
+function displayPoem(response) {
+  console.log("Generated Poem!");
+  //response.data.answer
   new Typewriter("#funny-quote", {
-    strings: "I see your face when I am dreaming",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateFy(event) {
+  event.preventDefault();
+
+  let instruction = document.querySelector("#words");
+  // Build the API URL
+  let apiKey = "db9df5cafd32a54ef0cf0c7bd2635oct";
+  let context =
+    "A humous AI who loves funny poem generate in four sentences in basic HTML. Make sure to be nice and follow the user instructions.";
+  let prompt = `User Instruction: your AI assistant,be nice and short to the point ${instruction.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  alert("Processing the funny quote...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+  // Call the API
+  axios.get(apiUrl).then(displayPoem);
+  // Display API funny poem
 }
 let funnyPoem = document.querySelector("#funny-poem");
 funnyPoem.addEventListener("submit", generateFy);
