746f264ef18a8c4b00227b744b428cabe3348f87
bcot-code <78989054+bcot-code@users.noreply.github.com>
2024-09-16 16:06:04 -0500

    Update Api and hidden form

--- src/index.js
+++ src/index.js
@@ -16,16 +16,19 @@ function generateFy(event) {
   // Build the API URL
   let apiKey = "db9df5cafd32a54ef0cf0c7bd2635oct";
   let context =
-    "A humous AI who loves funny poem generate in four sentences in basic HTML. Make sure to be nice and follow the user instructions.";
+    "A humorous AI who loves funny poem generate in four sentences in basic HTML. Make sure to be nice and follow the user instructions.";
   let prompt = `User Instruction: your AI assistant,be nice and short to the point ${instruction.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  alert("Processing the funny quote...");
+  //   alert("Processing the funny quote...");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
   // Call the API
   axios.get(apiUrl).then(displayPoem);
   // Display API funny poem
+  let hiding = document.querySelector("#funny-quote");
+  hiding.classList.remove("hide");
+  hiding.innerHTML = `Generate a Funny Poem ${instruction.value}`;
 }
 let funnyPoem = document.querySelector("#funny-poem");
 funnyPoem.addEventListener("submit", generateFy);
